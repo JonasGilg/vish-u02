7e4c7d28b5ec83c5c07df969ba2e86356e6fbdf9
TheJhonny007 <jonas.gilg@stud.hs-hannover.de>
2018-03-27 15:20:24 +0200

    single repaint per update

--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -1,41 +1,4 @@
-import javafx.beans.property.ReadOnlyObjectProperty
-import javafx.beans.property.ReadOnlyObjectWrapper
-import javafx.geometry.Point2D
-import javafx.scene.canvas.Canvas
-import javafx.scene.layout.Region
-import tornadofx.*
 import java.util.stream.IntStream
 
 fun parallelFor(range: IntRange, op: (Int) -> Unit) = IntStream.range(range.first, range.last).parallel().forEach(op)
 
-fun Region.sizeProperty(): ReadOnlyObjectProperty<Size> {
-	val property = ReadOnlyObjectWrapper(Size(width, height))
-
-	widthProperty().onChange { property.value = Size(it, height) }
-	heightProperty().onChange { property.value = Size(width, it) }
-
-	return property.readOnlyProperty
-}
-
-val Region.size get() = Size(width, height)
-
-fun Canvas.sizeProperty() : ReadOnlyObjectProperty<Size> {
-	val property = ReadOnlyObjectWrapper(Size(width, height))
-
-	widthProperty().onChange { property.value = Size(it, height) }
-	heightProperty().onChange { property.value = Size(width, it) }
-
-	return property.readOnlyProperty
-}
-
-val Canvas.size get() = Size(width, height)
-
-data class Size(val width: Double, val height: Double)
-
-operator fun Point2D.plus(other: Point2D): Point2D = this.add(other)
-operator fun Point2D.minus(other: Point2D): Point2D = this.subtract(other)
-
-operator fun Point2D.times(factor: Double): Point2D = this.multiply(factor)
-operator fun Point2D.div(dividend: Double): Point2D = this.multiply(1.0 / dividend)
-
-infix fun Point2D.dot(other: Point2D): Double = this.dotProduct(other)
--- src/main/kotlin/Window.kt
+++ src/main/kotlin/Window.kt
@@ -1,3 +1,6 @@
+import javafx.application.Platform
+import javafx.beans.Observable
+import javafx.geometry.Point2D
 import javafx.scene.image.WritableImage
 import javafx.scene.paint.Color
 import tornadofx.*
@@ -7,6 +10,8 @@ class AppStarter : App(MainView::class)
 class MainView : View("Bilinear Interpolation") {
 	private val dataController by inject<DataController>()
 
+	private var canvasSize: Point2D? = null
+
 	override val root = pane {
 		prefWidth = 800.0
 		prefHeight = 800.0
@@ -15,23 +20,31 @@ class MainView : View("Bilinear Interpolation") {
 			widthProperty().bind(this@pane.widthProperty())
 			heightProperty().bind(this@pane.heightProperty())
 
-			sizeProperty().onChange {
-				if (width > 0 && height > 0) {
-					val w = width.toInt()
-					val h = height.toInt()
+			val listener = { _: Observable ->
+				if (canvasSize == null) {
+					Platform.runLater {
+						val w = width.toInt()
+						val h = height.toInt()
 
-					val image = WritableImage(w, h)
+						val image = WritableImage(w, h)
 
-					parallelFor(0 until w) { x ->
-						for (y in 0 until h) {
-							val color = 0.5 * (dataController.getInterpolatedData(x / width, y / height) + 1.0)
-							image.pixelWriter.setColor(x, y, Color(color, color, color, 1.0))
+						parallelFor(0 until w) { x ->
+							for (y in 0 until h) {
+								val color = 0.5 * (dataController.getInterpolatedData(x / width, y / height) + 1.0)
+								image.pixelWriter.setColor(x, y, Color(color, color, color, 1.0))
+							}
 						}
-					}
 
-					graphicsContext2D.drawImage(image, 0.0, 0.0)
+						graphicsContext2D.drawImage(image, 0.0, 0.0)
+
+						canvasSize = null
+					}
 				}
+				canvasSize = Point2D(width, height)
 			}
+
+			widthProperty().addListener(listener)
+			heightProperty().addListener(listener)
 		}
 	}
 }
